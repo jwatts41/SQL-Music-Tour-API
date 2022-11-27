36367c75ed222bb7a639fd05d23e00cecb628dbb
jwatts41 <wattsjonovan@yahoo.com>
2022-11-27 15:17:15 -0500

    implemented the create route

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -23,6 +23,19 @@ bands.get('/:id', async (req, res) => {
     }
 })
 
+// CREATE A BAND
+bands.post('/', async (req, res) => {
+    try {
+        const newBand = await Band.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new band',
+            data: newBand
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 
 
 
