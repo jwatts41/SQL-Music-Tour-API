8e962f7fb8eaffa96597352958cb60a75ea54e8f
jwatts41 <wattsjonovan@yahoo.com>
2022-11-27 16:04:52 -0500

    put the dates in ascending order

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -5,7 +5,9 @@ const { Band } = db
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ]]
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
