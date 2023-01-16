913f56b0e1fae4b83afa6f24d073167bb5bd41f8
jwatts41 <wattsjonovan@yahoo.com>
2023-01-16 16:24:37 -0500

    updated bands controller

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,6 +1,6 @@
 const bands = require('express').Router()
 const db = require('../models')
-const { Band } = db
+const { Band, MeetGreet, Event, SetTime } = db
 const { Op } = require('sequelize')
 
 // FIND ALL BANDS
@@ -17,17 +17,36 @@ bands.get('/', async (req, res) => {
         res.status(500).json(error)
     }    
 })
+
+
 // FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
+bands.get('/:name', async (req, res) => {
     try {
         const foundBand = await Band.findOne({
-            where: { band_id: req.params.id }
+            where: { name: req.params.name  },
+            include: [ 
+                { 
+                    model: MeetGreet, 
+                    as: "meet_greets",
+                    include: { model: Event, as: "event" } ,
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                },
+                { 
+                    model: SetTime,
+                    as: "set_times",
+                    include: { model: Event, as: "event" },
+                    where: { name: { [Op.like]: `%${req.query.event ? req.query.event : ''}%` } }
+                }
+            ] 
         })
         res.status(200).json(foundBand)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+
+
+
 
 // CREATE A BAND
 bands.post('/', async (req, res) => {
